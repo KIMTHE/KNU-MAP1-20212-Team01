d43102482340dbd1ecee9a0fb5a4baffa6cecffe
KIMTHE <kcms2369@naver.com>
2021-11-14 19:58:54 +0900

    [REFACTOR] : modify variable name

--- android/app/src/main/java/com/jongsip/streetstall/util/PermissionUtil.kt
+++ android/app/src/main/java/com/jongsip/streetstall/util/PermissionUtil.kt
@@ -8,25 +8,36 @@ import androidx.core.content.ContextCompat
 import com.jongsip.streetstall.activity.CustomerMainActivity
 
 object PermissionUtil {
+
+    const val PERMISSION_CODE_ACCEPTED = 1
+    const val PERMISSION_CODE_NOT_AVAILABLE = 0
+
     // 위치권한요청
     fun requestLocationPermission(context: Context): Int {
-        if (ContextCompat.checkSelfPermission(context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(
                     context as Activity,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
             } else {
                 // request permission
-                ActivityCompat.requestPermissions(context,
+                ActivityCompat.requestPermissions(
+                    context,
                     arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    CustomerMainActivity.PERMISSION_CODE_ACCEPTED)
+                    PERMISSION_CODE_ACCEPTED
+                )
             }
         } else {
             // already granted
-            return CustomerMainActivity.PERMISSION_CODE_ACCEPTED
+            return PERMISSION_CODE_ACCEPTED
         }
         // not available
-        return CustomerMainActivity.PERMISSION_CODE_NOT_AVAILABLE
+        return PERMISSION_CODE_NOT_AVAILABLE
     }
 }
